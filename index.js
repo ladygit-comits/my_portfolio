846a65800973ea2bb914f327f2044d8bce68fd5e
Ladygit <135207340+ladygit-comits@users.noreply.github.com>
2024-08-14 11:15:21 +0300

    UI/UX
    Improve UI and UX

---
+++ index.js
@@ -0,0 +1,18 @@
+//Get the button:
+let scrollTopBtn = document.getElementById("scrollTopBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    scrollTopBtn.style.display = "block";
+  } else {
+    scrollTopBtn.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+scrollTopBtn.addEventListener("click", function(){
+  window.scrollTo({top: 0, behavior: 'smooth'});
+});
